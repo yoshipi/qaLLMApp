3aa9919cefc51771f97bd548711ad7646d276b9b
yoshipi <yoshidas@yoshidasadarinnoMacBook-Pro.local>
2025-01-01 19:01:49 +0900

    add json format sample

---
+++ sample/langChainTutorial/cook/pydanticOutputParserLLM.py
@@ -0,0 +1,23 @@
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from Recipe import Recipe
+from langchain_openai import AzureChatOpenAI
+    
+output_parser = PydanticOutputParser(pydantic_object=Recipe)
+
+format_instructions = output_parser.get_format_instructions()
+
+prompt = ChatPromptTemplate.from_messages(
+    [("system", "ユーザーが入力した料理のレシピを考えてください。¥n¥n"
+     "{format_instructions}"),
+     ("human","{dish}")]
+)
+
+prompt_with_format_instructions = prompt.partial(format_instructions=format_instructions)
+prompt_value = prompt_with_format_instructions.invoke({"dish": "カレー"})
+
+model = AzureChatOpenAI(
+    azure_deployment="gpt-4o",
+    openai_api_version="2024-08-01-preview")
+ai_message = model.invoke(prompt_value)
+print(ai_message.content)
--- sample/langChainTutorial/language/language.py
+++ sample/langChainTutorial/language/language.py
@@ -2,11 +2,11 @@ import getpass
 import os
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 system_template = "Translate the following from English into {language}"
 prompt_template = ChatPromptTemplate.from_messages(
-    [("system", system_template), ("user", "{text}")]
+    [("system", system_template), MessagesPlaceholder("chat_history",optional=True),("user", "{text}")]
 )
 
 model = AzureChatOpenAI(
@@ -14,13 +14,8 @@ model = AzureChatOpenAI(
     openai_api_version="2024-08-01-preview",
 )
 
-
-messages = [
-    SystemMessage("Translate the following from English into Italian"),
-    HumanMessage("hi!"),
-]
-
 prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
 
+print("This is prompt:" + str(prompt))
 response = model.invoke(prompt)
 print(response.content)
