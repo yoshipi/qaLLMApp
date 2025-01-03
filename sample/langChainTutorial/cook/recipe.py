f49916566e02955c811b68ad8517719c8f2c2056
yoshipi <world.love.enjoy@gmail.com>
2025-01-03 16:16:56 +0900

    Delete sample/langChainTutorial directory

--- sample/langChainTutorial/cook/recipe.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel, Field
-class Recipe(BaseModel):
-    ingredients: list[str] = Field(description="ingredients of dish")
-    steps: list[str] = Field(description="steps to make dish")
--- sample/langChainTutorial/cook/strOutputParser.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.messages import AIMessage
-from langchain_core.output_parsers import StrOutputParser
-
-output_parser = StrOutputParser()
-ai_message = AIMessage(content="カレーを作る")
-output = output_parser.invoke(ai_message)
-print(type(output))
-print(output)
